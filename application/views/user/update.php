04258cc24f0b3c3e3a43a86a4a24c87f97255f88
septrinur <semuth03@gmail.com>
2020-11-09 10:17:55 +0700

    devs

--- application/views/user/update.php
+++ application/views/user/update.php
@@ -103,18 +103,27 @@
                                     <select class="form-control custom-select" name="level">
                                         <?php if (isset($param)) { 
                                             if ($param['level'] == '2') { ?>
+                                            <option value="1">Super Admin</option>
                                             <option value="2" selected>Admin QR</option>
                                             <option value="3">Maker QR</option>
                                             <option value="4">Admin Aplikasi</option>
                                         <?php }elseif ($param['level'] == '3') { ?>
+                                            <option value="1">Super Admin</option>
                                             <option value="2">Admin QR</option>
                                             <option value="3" selected>Maker QR</option>
                                             <option value="4">Admin Aplikasi</option>   
                                         <?php }elseif ($param['level'] == '4') { ?>
+                                            <option value="1">Super Admin</option>
                                             <option value="2">Admin QR</option>
                                             <option value="3">Maker QR</option>
                                             <option value="4" selected>Admin Aplikasi</option>   
+                                        <?php } elseif ($param['level'] == '1') { ?>
+                                            <option value="1" selected>Super Admin</option>
+                                            <option value="2">Admin QR</option>
+                                            <option value="3">Maker QR</option>
+                                            <option value="4">Admin Aplikasi</option>   
                                         <?php } 
+
 
                                         }else{ ?>
                                             <option value="2">Admin QR</option>
